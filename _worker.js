39ff37ef1184f939a17da7d2f42c3d3c1882aca2
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2024-04-09 23:40:58 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '581b9250-aabf-481e-a3f8-18e0d88ed220';
 
-let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = '';
 
 
 if (!isValidUUID(userID)) {
